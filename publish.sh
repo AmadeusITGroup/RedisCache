fa3a2dad1672a5985d9d8cf8045bb23f49814f52
Pierre Cart-Grandjean <pcart-grandjean@amadeus.com>
2025-03-11 11:54:28 +0100

    Fix typo in publish

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Exit in case of error
+set -e
+
 # Check if we are on the main branch
 branch=$(git branch --show-current)
 if [[ $branch != "main" ]]; then
@@ -20,7 +23,7 @@ if [[ $status != "## main...origin/main" ]]; then
 fi
 
 # If the current version is the latest one available on PyPI, we need to bump it
-pypi_version=$(poetry search executiontime | grep --perl-regexp --only-matching "([0-9]+\.[0-9]+\.[0-9]+)" | tail -n 1)
+pypi_version=$(poetry search rediscache | grep --perl-regexp --only-matching "([0-9]+\.[0-9]+\.[0-9]+)" | tail -n 1)
 local_version=$(poetry version --short)
 if [[ "${pypi_version}" == "${local_version}" ]]; then
     poetry version patch
